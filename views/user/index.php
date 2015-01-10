77084899faad62831a8335ab281b51bf97823c98
Julio César Barrera Andrade <staff@codebit.com.mx>
2015-01-10 10:38:29 -0600

    Cambio de leyenda

--- views/user/index.php
+++ views/user/index.php
@@ -19,7 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
 	<?php //echo $this->render('_search', ['model' => $searchModel]); ?>
 
 	<p>
-		<?= Html::a('<i class="glyphicon glyphicon-plus-sign"></i> ' . Yii::t('auth.user', 'Crear usuario'), ['create'], ['class' => 'btn btn-success']) ?>
+		<?= Html::a('<i class="glyphicon glyphicon-plus-sign"></i> ' . Yii::t('auth.user', 'Dar de alta un usuario'), ['create'], ['class' => 'btn btn-success']) ?>
 	</p>
 
 	<?php echo GridView::widget([
